f048dd79605cc15013294e85cdf265896a733e25
samuel silva <prog.samdev@gmail.com>
2023-12-13 18:43:59 -0300

    chore(api): first commit

--- cmd/worker/main.go
+++ cmd/worker/main.go
@@ -3,6 +3,11 @@ package main
 import (
 	"GoDriver/internal/bucket"
 	"GoDriver/internal/queue"
+	"bytes"
+	"compress/gzip"
+	"fmt"
+	"io"
+	"log"
 	"os"
 	"time"
 
@@ -35,9 +40,51 @@ func main() {
 		BucketUpload:   "aprenda-golang-drive-gzip",
 	}
 
-	b := bucket.New(bucket.AwsProvider, bcfg)
+	b, err := bucket.New(bucket.AwsProvider, bcfg)
+
+	if err != nil {
+		panic(err)
+	}
 
 	for msg := range ch {
+		src := fmt.Sprintf("%s/%s", msg.Path, msg.Filename)
+		dst := fmt.Sprintf("%d_%s", msg.ID, msg.Filename)
+		file, err := b.Download(src, dst)
+		if err != nil {
+			log.Printf("ERROR: %v", err)
+			continue
+		}
+
+		body, err := io.ReadAll(file)
+		if err != nil {
+			log.Printf("ERROR: %v", err)
+		}
+		var buf bytes.Buffer
+		zw := gzip.NewWriter(&buf)
+		_, err = zw.Write((body))
+		if err != nil {
+			log.Printf("ERROR: %v", err)
+		}
+
+		if err := zw.Close(); err != nil {
+			log.Printf("ERROR %v", err)
+			continue
+		}
+
+		zr, err := gzip.NewReader(&buf)
+		if err != nil {
+			log.Printf("ERROR: %v", err)
+		}
+
+		err = b.Upload(zr, src)
+		if err != nil {
+			log.Printf("ERROR: %v", err)
+		}
+
+		os.Remove(dst)
+		if err != nil {
+			log.Printf("ERROR: %v", err)
+		}
 
 	}
 
