f7065d5a99c9113be86af5b7402001676e7d3b7e
samuel silva <prog.samdev@gmail.com>
2023-12-16 14:18:06 -0300

    chore(api): Delete users

---
+++ internal/users/delete.go
@@ -0,0 +1,36 @@
+package users
+
+import (
+	"database/sql"
+	"net/http"
+	"strconv"
+	"time"
+
+	"github.com/go-chi/chi"
+)
+
+func (h *handler) Delete(rw http.ResponseWriter, r *http.Request) {
+
+	id, err := strconv.Atoi(chi.URLParam(r, "id"))
+
+	if err != nil {
+		http.Error(rw, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	err = Delete(h.db, id)
+	if err != nil {
+		http.Error(rw, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	rw.Header().Add("Content-Type", "application/json")
+}
+
+func Delete(db *sql.DB, id int) error {
+
+	stmt := `update "users" set "deleted"= true, "modified_at"=$2 WHERE id = $3`
+	_, err := db.Exec(stmt, time.Now(), id)
+	return err
+
+}
